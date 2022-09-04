a3ac1c7b5839e257b585f3fbcccfa2c3532b989c
Luuq <lukasz2zagaja@gmail.com>
2022-09-04 15:06:28 +0200

    FrameRateIndependent Thrusting

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] float mainThrust = 100f;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -22,7 +23,7 @@ public class Movement : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.Space))
         {
-            rb.AddRelativeForce(Vector3.up);
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
         }
         
     }
