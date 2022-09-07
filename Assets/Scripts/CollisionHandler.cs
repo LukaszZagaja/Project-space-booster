85d7e311c0dbc79952385b34de656f92ef79b568
Luuq <lukasz2zagaja@gmail.com>
2022-09-07 17:49:50 +0200

    Levels
    added 2nd scene that is also 2nd level, respawning works on both, level changing works on all scenes, if u get to finish on last level, then you start all over from level 1

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -14,7 +14,7 @@ public class CollisionHandler : MonoBehaviour
                 }
             case "Finish":
                 {
-                    Debug.Log("Congrats, you finished!");
+                    LoadNextLevel();
                     break;
                 }
             case "Fuel":
@@ -33,6 +33,16 @@ public class CollisionHandler : MonoBehaviour
         {
             int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
             SceneManager.LoadScene(currentSceneIndex);
+        }
+        void LoadNextLevel()
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = currentSceneIndex + 1;
+            if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+            {
+                nextSceneIndex = 0;
+            }
+            SceneManager.LoadScene(nextSceneIndex);
         }
     }
 }
