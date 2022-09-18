380effe028915794d3df657c16b3cc0434dd460d
Luuq <lukasz2zagaja@gmail.com>
2022-09-18 19:51:27 +0200

    OscillatorScriptWorking
    oscillator script working on objects, NaN bug eliminated

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
     Vector3 startingPosition;
-    [SerializeField]Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;    
+    [SerializeField] Vector3 movementVector;
+    float movementFactor;
+    [SerializeField] float period = 2f;
     void Start()
     {
         startingPosition = transform.position;
@@ -14,6 +15,17 @@ public class Oscillator : MonoBehaviour
 
     void Update()
     {
+        if (period <= Mathf.Epsilon)
+        {
+            return;
+        }
+        float cycles = Time.time / period; // continually growing over time
+
+        const float tau = Mathf.PI * 2; // constant value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from 1 to -1
+
+        movementFactor = (rawSinWave + 1f) / 2f; // recalculatego to go from 1 to 0 so its cleaner
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
